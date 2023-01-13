0e682a645edf98c63ca9545c10ff223632e71c36
kent@legion <164713332@qq.com>
2023-01-13 09:47:05 +0000

    Add weeshop_alpha_components module as git repository.

--- script/RepalceModulesToGitRepositories.sh
+++ script/RepalceModulesToGitRepositories.sh
@@ -5,6 +5,10 @@ cd ./web/modules/contrib || exit
 rm -rf ./weeshop_demo
 git clone git@github.com:weeshop/weeshop_demo.git
 
+rm -rf ./weeshop_alpha_components
+git clone git@github.com:weeshop/weeshop_alpha_components.git
+
+
 cd ../../themes/contrib || exit
 
 rm -rf ./weeshop_alpha
