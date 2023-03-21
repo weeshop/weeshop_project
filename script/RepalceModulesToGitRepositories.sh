fbe4aca46e9497e2fc30f49eb787d2a23735a7ed
kent@legion <164713332@qq.com>
2023-03-21 07:42:11 +0000

    Fix config schema.

--- script/RepalceModulesToGitRepositories.sh
+++ script/RepalceModulesToGitRepositories.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+cp -r ./script/config ./web/modules/contrib/facets/modules/facets_rest
+
 cd ./web/modules/contrib || exit
+
+rm -rf ./product_adverts
+git clone git@github.com:solody/product_adverts.git
+
+rm -rf ./adverts
+git clone git@github.com:solody/adverts.git
 
 rm -rf ./enhanced_product
 git clone git@github.com:solody/enhanced_product.git
 
 rm -rf ./enhanced_store
 git clone git@github.com:solody/enhanced_store.git
-
 
 rm -rf ./weeshop_demo
 git clone git@github.com:weeshop/weeshop_demo.git
@@ -18,6 +25,8 @@ git clone git@github.com:weeshop/weeshop_alpha_components.git
 rm -rf ./weeshop_molla_supports
 git clone git@github.com:weeshop/weeshop_molla_supports.git
 
+
+
 cd ../../themes/contrib || exit
 
 rm -rf ./weeshop_alpha
@@ -25,6 +34,7 @@ git clone git@github.com:weeshop/weeshop_alpha.git
 
 rm -rf ./weeshop_molla
 git clone git@github.com:weeshop/weeshop_molla.git
+
 
 
 cd ../../profiles/contrib || exit
