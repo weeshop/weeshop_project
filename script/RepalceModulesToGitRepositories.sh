97df0b8e1bae2ad5c18ac94e14fba3ccdd365d69
kent@legion <164713332@qq.com>
2023-03-24 06:58:38 +0000

    Fix dependency.

--- script/RepalceModulesToGitRepositories.sh
+++ script/RepalceModulesToGitRepositories.sh
@@ -5,6 +5,9 @@ cp -r ./script/config ./web/modules/contrib/facets/modules/facets_rest
 
 cd ./web/modules/contrib || exit
 
+rm -rf ./migrate_inline_image
+git clone git@github.com:solody/migrate_inline_image.git
+
 rm -rf ./product_adverts
 git clone git@github.com:solody/product_adverts.git
 
