83827483911a0e75eab7c13e3859c58573fc3fcf
kent@legion <164713332@qq.com>
2023-07-21 06:19:20 +0000

    Fix path bug.

--- scripts/RepalceModulesToGitRepositories.sh
+++ scripts/RepalceModulesToGitRepositories.sh
@@ -4,7 +4,7 @@ source ./scripts/TryCommand.sh
 
 
 # https://www.drupal.org/project/facets/issues/3349266
-cp -r ./script/config ./web/modules/contrib/facets/modules/facets_rest
+cp -r ./scripts/config ./web/modules/contrib/facets/modules/facets_rest
 
 cd ./web/modules/contrib || exit
 
