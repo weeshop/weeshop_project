708a30d3b7fdedaee141fc30aec270a82e06d628
kent <164713332@qq.com>
2025-12-26 11:22:04 +0000

    Backup.

--- scripts_setting.sh
+++ scripts_setting.sh
@@ -2,6 +2,13 @@
 # Sample settings for scripts. Copy this file to scripts/../scripts_setting.sh and modify as needed.
 export DRUPAL_PATH=./drupal
 export DRUPAL_MODULES_TO_REPLACE_TO_GIT=(
+"github.com solody drupal-migrate_inline_image 1.0.x migrate_inline_image"
+"github.com solody drupal-product_adverts 1.0.x product_adverts"
+"github.com solody drupal-adverts 1.0.x adverts"
+"github.com solody drupal-enhanced_product 1.0.x enhanced_product"
+"github.com solody drupal-enhanced_store 1.0.x enhanced_store"
+"github.com weeshop weeshop_demo 2.0.x weeshop_demo"
+"github.com weeshop weeshop_molla_supports 1.0.x weeshop_molla_supports"
 )
 
 export DRUPAL_THEMES_TO_REPLACE_TO_GIT=(
